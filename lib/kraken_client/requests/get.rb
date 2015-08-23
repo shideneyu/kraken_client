152b16bec1c3b7a12ea57b56a171cbcd4b598f4f
sidney <shideneyu@gmail.com>
2015-08-23 23:03:36 +0200

    Fix: Public endpoints formatting

--- lib/kraken_client/requests/get.rb
+++ lib/kraken_client/requests/get.rb
@@ -5,7 +5,13 @@ module KrakenClient
       def call(url, params, *)
         super
 
-        HTTParty.get(url, query: params)
+        HTTParty.get(url, query: parsed_params(params))
+      end
+
+      private
+
+      def parsed_params(params)
+        params.is_a?(Hash) ? params : { pair: params }
       end
 
     end
