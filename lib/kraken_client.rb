0af47eb9590c1c80b8465cc359ef9c2847bf34a3
sidney <shideneyu@gmail.com>
2015-08-23 22:32:27 +0200

    Fix: typo

--- lib/kraken_client.rb
+++ lib/kraken_client.rb
@@ -1,6 +1,6 @@
 ruby_project_files = Dir[File.join(File.dirname(__FILE__), '**', '*.rb')]
 
-ruby_project_files.sort_by!{ |m| m.downcase }.each do |path|
+ruby_project_files.sort_by!{ |file_name| file_name.downcase }.each do |path|
   require_relative path
 end
 
